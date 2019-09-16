756dcb7f138efde543c8963f12a7b4652643b9fa
54zhuxs <664922015@qq.com>
2019-09-16 17:54:50 +0800

    新增第一个ASP.NET Core学习项目

---
+++ Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly List<Content> contents;
+        public ContentController(IOptionsSnapshot<List<Content>> option)
+        {
+            contents = option.Value;
+        }
+        // GET: Content
+        public ActionResult Index()
+        {
+            /*
+            var contents = new List<Content>();
+            for (int i = 1; i < 11; i++)
+            {
+                contents.Add(new Models.Content { Id = i, title = $"{i}标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-1) });
+            }
+            return View(new ContentViewModel { Contents = contents });
+            */
+            return View(new ContentViewModel { Contents = contents });
+        }
+
+        // GET: Content/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Content/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Content/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Content/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Content/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Content/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Content/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
